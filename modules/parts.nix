19b960c6074f9b63ef027997d1a41fd45964a5a9
ferret <73801176+ferretirl@users.noreply.github.com>
2026-06-20 00:24:17 +0200

    attempt at fixing build error

--- modules/parts.nix
+++ modules/parts.nix
@@ -1,10 +1,12 @@
-{
-  config = {
-    systems = [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ];
-  };
+{ inputs, ... }: {
+  imports = [
+    inputs.home-manager.flakeModules.home-manager
+  ];
+
+  config.systems = [ 
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+  ];
 }
